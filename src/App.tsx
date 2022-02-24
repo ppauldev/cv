537650932fc49049d1951cd7c4d5ca22f93bf09b
Phillip Paul <phillippaul@gmx.de>
2022-02-24 20:38:42 +0100

    Format for A4

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import styles from './App.module.css';
 function App() {
   return (
     <div className={styles.App}>
-      Test
+      <div className={styles.cvWrapper}>
+        Test
+      </div>
     </div>
   );
 }
