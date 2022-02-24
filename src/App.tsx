cc48d0bfae5e14ed14f57242494002f6f15e1426
Phillip Paul <phillippaul@gmx.de>
2022-02-24 21:24:31 +0100

    Add initial components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,89 @@
 import styles from './App.module.css';
 
-function App() {
+const App = (): JSX.Element => {
   return (
     <div className={styles.App}>
       <div className={styles.cvWrapper}>
-        Test
+        <CVContent />
       </div>
     </div>
   );
+}
+
+const CVContent = (): JSX.Element => {
+  return (
+    <div className={styles.cvContent}>
+      <Header />
+      <Summary />
+      <Body />
+    </div>
+  )
+}
+const Header = (): JSX.Element => {
+  return (
+    <>
+      Header info
+    </>
+  )
+}
+
+const Summary = (): JSX.Element => {
+  return (
+    <>
+      Summary
+    </>
+  )
+}
+
+const Body = (): JSX.Element => {
+  return (
+    <>
+      <Experience />
+      <Info />
+    </>
+  )
+}
+
+const Experience = (): JSX.Element => {
+  return (
+    <>
+      Experience
+    </>
+  )
+}
+
+const Info = (): JSX.Element => {
+  return (
+    <>
+      <Education />
+      <Awards />
+      <Languages />
+    </>
+  )
+}
+
+const Education = (): JSX.Element => {
+  return (
+    <>
+      Education
+    </>
+  )
+}
+
+const Awards = (): JSX.Element => {
+  return (
+    <>
+      Awards
+    </>
+  )
+}
+
+const Languages = (): JSX.Element => {
+  return (
+    <>
+      Languages
+    </>
+  )
 }
 
 export default App;
