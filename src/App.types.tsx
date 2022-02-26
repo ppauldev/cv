b7d6fb403feeea73fb8a805ae6c19aaeb94466b5
Phillip Paul <phillippaul@gmx.de>
2022-02-26 17:38:01 +0100

    Add rest types

--- src/App.types.tsx
+++ src/App.types.tsx
@@ -1,32 +1,69 @@
 export type TEducationData = {
   label: string,
-  data: Array<object>
+  data: [TSchool]
+}
+
+export type TSchool = {
+  degree: string,
+  name: string,
+  duration: string
 }
 
 export type TJobsData = {
   label: string,
-  data: Array<object>
+  data: [TJob]
+}
+
+export type TJob = {
+  title: string,
+  tenure: string,
+  description: string,
+  tasks: [TJobTask],
+  techstack?: [string]
+}
+
+export type TJobTask = {
+  description: string
 }
 
 export type TLanguagesData = {
   label: string,
-  data: Array<object>
+  data: [TLanguage]
+}
+
+export type TLanguage = {
+  language: string,
+  level: string
 }
 
 export type TPersonData = {
   label: string,
-  data: Array<object>
+  data: [TPersonInfo]
+}
+
+export type TPersonInfo = {
+  fullname: string,
+  role: string,
+  email: string,
+  emailsubject: string,
+  phone: string,
+  linkedin: string,
+  github: string
 }
 
 export type TSummaryData = {
   label: string,
-  data: Array<object>
+  data: [TSummaryText]
+}
+
+export type TSummaryText = {
+  summary: string
 }
 
 export type TCVData = {
-  educationData: object,
-  jobsData: object,
-  languagesData: object,
-  personData: object,
-  summaryData: object
+  educationData: TEducationData,
+  jobsData: TJobsData,
+  languagesData: TLanguagesData,
+  personData: TPersonData,
+  summaryData: TSummaryData
 }
