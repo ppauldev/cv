94e95f7e459e19650547a8aba042a244bf7bb21a
Phillip Paul <ppaul_dev@t-online.de>
2025-01-29 19:04:16 +0100

    Clean repo

--- dist/api/loadData.js
+++
@@ -1,7 +0,0 @@
-import * as cvdataJSON from "../cvdata.json";
-// Interface for data loading, here via JSON
-export var loadData = function () {
-    if (!cvdataJSON)
-        return {};
-    return cvdataJSON;
-};
--- dist/helper/utils.js
+++
@@ -1,1 +0,0 @@
-export var isEmptyObject = function (data) { return Object.keys(data).length === 0 && data.constructor === Object; };
