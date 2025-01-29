fa5cf3f3d5a80dbd668f88cc6d493cafd8cacb2e
Phillip Paul <ppaul_dev@t-online.de>
2025-01-29 20:29:04 +0100

    Release build

--- src/api/loadData.tsx
+++
@@ -1,9 +0,0 @@
-import * as cvdataJSON from "../cvdata.json"
-import * as Types from "../App.types"
-
-// Interface for data loading, here via JSON
-export const loadData = (): Types.TCVData | {} => {
-  if (!cvdataJSON) return {}
-
-  return cvdataJSON
-}
